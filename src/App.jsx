777b2a5860ca793a978539cdba55a0ff79b9b977
Everth Paredes <epe0413@gmail.com>
2022-05-12 10:35:42 -0600

    Creamos y agrupamos el routing del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import IniciarSesion from './layout/IniciarSesion'
 import Layout from './layout/Layout'
 import Inicio from './pages/Inicio'
-import LoginForm from './pages/LoginForm'
+import NuevoCliente from './pages/NuevoCliente'
+import EditarCliente from './pages/EditarCliente'
 
 
 function App() {
@@ -12,12 +12,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<IniciarSesion/>}>
-          <Route index element={<LoginForm/>}/>
-        </Route>
-
         <Route path="/clientes" element={<Layout/>}>
           <Route index element={<Inicio/>}/>
+          <Route path='nuevo' element={<NuevoCliente/>}/>
+          <Route path='editar/:id' element={<EditarCliente/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
