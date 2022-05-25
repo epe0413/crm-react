d40defe610f1f88846ef05219bc1097c7e72a029
Everth Paredes <epe0413@gmail.com>
2022-05-25 16:43:16 -0600

    Se agrego formik y yup al proyecto y se creo el primer formulario con formik

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,84 @@
 import React from 'react'
+import { Formik, Form, Field} from 'formik'
 
 const Formulario = () => {
   return (
     <div className='bg-white mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto'>
         <h1 className='text-gray-600 font-bold text-xl uppercase text-center'>Agregar cliente</h1>
+
+        <Formik>
+            <Form
+                className='mt-10'
+            >
+                <div className='mb-4'>
+                    <label
+                        className='text-gray-800'
+                        htmlFor="nombre"
+                    >Nombre</label>
+                    <Field
+                        id="nombre"
+                        type="text"
+                        className="mt-2 block w-full p-3 bg-gray-50 rounded-md"
+                        placeholder="Nombre del cliente"
+                    />
+                </div>
+                <div className='mb-4'>
+                    <label
+                        className='text-gray-800'
+                        htmlFor="empresa"
+                    >Empresa</label>
+                    <Field
+                        id="empresa"
+                        type="text"
+                        className="mt-2 block w-full p-3 bg-gray-50 rounded-md"
+                        placeholder="Empresa del cliente"
+                    />
+                </div>
+                <div className='mb-4'>
+                    <label
+                        className='text-gray-800'
+                        htmlFor="email"
+                    >Email</label>
+                    <Field
+                        id="email"
+                        type="email"
+                        className="mt-2 block w-full p-3 bg-gray-50 rounded-md"
+                        placeholder="Email del cliente"
+                    />
+                </div>
+                <div className='mb-4'>
+                    <label
+                        className='text-gray-800'
+                        htmlFor="telefono"
+                    >Telefóno</label>
+                    <Field
+                        id="telefono"
+                        type="tel"
+                        className="mt-2 block w-full p-3 bg-gray-50 rounded-md"
+                        placeholder="Telefóno del cliente"
+                    />
+                </div>
+                <div className='mb-4'>
+                    <label
+                        className='text-gray-800'
+                        htmlFor="notas"
+                    >Notas</label>
+                    <Field
+                        as="textarea"
+                        id="notas"
+                        type="text"
+                        className="mt-2 block w-full p-3 bg-gray-50 rounded-md h-40"
+                        placeholder="Notas del cliente"
+                    />
+                </div>
+
+                <input
+                    type="submit"
+                    value="Agregar Cliente"
+                    className='mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold text-lg rounded-md'
+                />
+            </Form>
+        </Formik>
     </div>
   )
 }
